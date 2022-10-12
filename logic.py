063ff39c1a8f057ca1587deb4a564535bcbe9683
Vladimir Shukhov <81926456+arfshukhov@users.noreply.github.com>
2022-10-13 01:20:58 +0500

    Add files via upload

--- logic.py
+++ logic.py
@@ -1,15 +1,21 @@
 def symbol_add(symbol: str, place):
-    text = place.text() + symbol
-    place.setText(text)
+    pos = place.cursorPosition()
+    text = list(place.text())
+    text.insert(pos, symbol)
+    place.setText("".join(text))
+    place.setCursorPosition(pos+1)
 
 
 def execution(place):
     expression = str(place.text())
     expression=expression.replace("√", "sqrt")
     expression=expression.replace("sqr", "pow")
-    print(expression)
-    exec(f"from math import *; result = {expression}; place.setText(str(result))")
-
+    try:
+        exec(f"from math import *; result = {expression}; place.setText(str(result))")
+    except ZeroDivisionError:
+        place.setText("You can not divide by zero!")
+    except SyntaxError:
+        place.setText("Invalid sintax. Check your expression...")
 
 def break_expression(place):
     place.setText("")
--- main.py
+++ main.py
@@ -10,6 +10,16 @@ import sys
 from logic import *
 
 
+
+@dataclass
+class Style:
+    background = "background-color: #464c57"
+    button_style = """QPushButton
+                    {background-color: #216afc; border-radius: 10px; font: bold 14px;min-width: 3em; padding: 1px;
+                    color: #212121;} QPushButton:pressed { background-color: #194294; border-radius: 10px;
+                    font: bold 14px;min-width: 3em; padding: 1px; color: #212121;}"""
+
+
 class Button:
     def __init__(self, text, function, arg, place):  # arg will be symbol ("1", "+", "/", ...), or fully expression
         super().__init__()
@@ -24,22 +34,24 @@ class Button:
 
 
 
-
-
 class Buttons:
     def __init__(self, place):
         self.buttons: list[Button]
         self.buttons = [
+            [Button("log(x)", symbol_add, "log()", place).button, Button("sin(α)", symbol_add, "sin()", place).button,
+             Button("cos(α)", symbol_add, "cos()", place).button, Button("tg(α)", symbol_add, "tg()", place).button,
+             Button(".", symbol_add, ".", place).button],
+
             [Button("7", symbol_add, "7", place).button, Button("8", symbol_add, "8", place).button,
              Button("9", symbol_add, "9", place).button, Button("+", symbol_add, "+", place).button,
-             Button("7", symbol_add, "7", place).button],
+             Button("-", symbol_add, "-", place).button],
 
             [Button("4", symbol_add, "4", place).button, Button("5", symbol_add, "5", place).button,
              Button("6", symbol_add, "6", place).button, Button("*", symbol_add, "*", place).button,
              Button("/", symbol_add, "/", place).button],
 
             [Button("1", symbol_add, "1", place).button, Button("2", symbol_add, "2", place).button,
-             Button("3", symbol_add, "3", place).button, Button("x²", symbol_add, "sqr()", place).button,
+             Button("3", symbol_add, "3", place).button, Button("xʸ", symbol_add, "sqr(x, y)", place).button,
              Button("√", symbol_add, "√()", place).button],
 
             [NotImplemented, Button("0", symbol_add, "0", place).button,
@@ -49,28 +61,44 @@ class Buttons:
         ]
 
 
-class Window(QWidget):
+class Window(QWidget, Style):
     def __init__(self):
         QWidget.__init__(self)
+        self.setGeometry(100, 100, 300, 300)
+        self.setStyleSheet(Style.background)
+
         self.entry = QLineEdit("", self)
-        self.entry.setText("")
+        self.entry.setGeometry(0, 0, 300, 33)
+        self.entry.setStyleSheet("background-color: lightgreen; border-radius: 10px; font: bold 14px;")
         self.entry.show()
 
         self.grid = QGridLayout(self)
+
+        self.lay = QWidget(self)
+        self.lay.setLayout(self.grid)
+        self.lay.setGeometry(-5, 35, 310, 280)
+
 
         self.buttons = Buttons(self.entry).buttons
 
         for i, m in enumerate(self.buttons):
             for k, d in enumerate(m):
-                if (i + 1, k + 1) == (4, 1):
+                if (i + 1, k + 1) == (5, 1):
                     self.btn1 = QPushButton("CE")
+                    #self.btn1.sizePolicy.setVerticalStretch()
                     self.btn1.clicked.connect(partial(break_expression, self.entry))
                     self.grid.addWidget(self.btn1, i + 1, k + 1)
-                elif (i + 1, k + 1) == (4, 3):
+                    self.btn1.setStyleSheet(Style.button_style)
+
+                elif (i + 1, k + 1) == (5, 3):
                     self.btn2 = QPushButton("=")
                     self.btn2.clicked.connect(partial(execution, self.entry))
                     self.grid.addWidget(self.btn2, i + 1, k + 1)
+                    self.btn2.setStyleSheet(Style.button_style)
+                    self.btn2.resize(10, 10)
                 else:
+                    d.setStyleSheet(Style.button_style)
+                    d.resize(50, 50)
                     self.grid.addWidget(d, i+1, k+1)
 
         self.show()
